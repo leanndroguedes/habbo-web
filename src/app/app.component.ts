a0dda2bf1574823f7b1be80b26f5b42b5e51c996
Leandro Guedes <leandroguedes@protonmail.com>
2021-02-25 11:28:16 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'habbo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'habbo-web';
+}

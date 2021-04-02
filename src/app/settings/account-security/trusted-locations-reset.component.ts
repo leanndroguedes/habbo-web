35d0e23a96793c6a278b37ec9f629d83cf9d3b7a
Leandro Guedes <leandroguedes@protonmail.com>
2021-04-01 22:43:27 -0300

    trusted locations reset notification added

--- src/app/settings/account-security/trusted-locations-reset.component.ts
+++ src/app/settings/account-security/trusted-locations-reset.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { NotifierService } from 'src/app/shared/notifier.service';
 
 @Component({
   selector: 'habbo-trusted-locations-reset',
@@ -9,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 export class TrustedLocationsResetComponent implements OnInit {
   inProgress = false;
 
-  constructor() { }
+  constructor(
+    private translate: TranslateService,
+    private notifier: NotifierService
+  ) { }
+
+  action(): void {
+    this.notifier.success('ACCOUNT_SECURITY_TRUSTED_LOGINS_RESET_OK');
+    this.notifier.error('ERROR_SERVER');
+    this.inProgress = false;
+  }
 
   reset(): void {
+    this.inProgress = true;
+    this.translate.get('ACCOUNT_SECURITY_RESET_LOCATIONS_CONFIRMATION').subscribe((res: string) => {
+      confirm(res) ? this.action() : this.inProgress = false;
+    });
   }
 
   ngOnInit(): void {
